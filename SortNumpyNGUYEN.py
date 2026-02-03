a0018ed77caeea2b8bd7840fa18de7d195132c4f
baht21 <25520671@gm.uit.edu.vn>
2026-02-03 21:52:15 +0700

    Update SortNumpyNGUYEN.py

--- SortNumpyNGUYEN.py
+++ SortNumpyNGUYEN.py
@@ -1,11 +1,11 @@
-import numpy as np
 import sys
 import time
+import numpy as np
 sys.stdin=open("input_1.inp","r")
 sys.stdout=open("output.out","w")
-start=time.perf_counter()
 n=int(input())
-a=np.array(input().split(),dtype=int)
-b=np.sort(a)
+a=np.array(list(map(int,input().split())))
+start=time.perf_counter()
+a.sort()
 end=time.perf_counter()
 print(round((end-start)*1000,6))
