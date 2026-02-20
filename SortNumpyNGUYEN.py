5bcd4eeebae2072039a233e133f4630671a2f492
baht21 <25520671@gm.uit.edu.vn>
2026-02-20 17:23:22 +0700

    Update files

--- SortNumpyNGUYEN.py
+++ SortNumpyNGUYEN.py
@@ -2,7 +2,7 @@ import sys
 import time
 import numpy as np
 sys.stdin=open("input_1.inp","r")
-#sys.stdout=open("output.out","w")
+sys.stdout=open("output.out","w")
 n=int(input())
 a=np.array(list(map(int,input().split())))
 start=time.perf_counter()
